a7911786fd269929b363c2602a955ed22d6f8fb7
Ping Xiao <pxiao@taosdata.com>
2021-01-13 16:45:32 +0800

    [TD-1930]<test>: add test case

--- tests/pytest/query/query.py
+++ tests/pytest/query/query.py
@@ -23,6 +23,8 @@ class TDTestCase:
         tdLog.debug("start to execute %s" % __file__)
         tdSql.init(conn.cursor(), logSql)
 
+        self.ts = 1538548685000
+
     def run(self):
         tdSql.prepare()
 
@@ -77,7 +79,25 @@ class TDTestCase:
         tdSql.checkRows(1)
         tdSql.checkData(0, 0, 1)
 
+        ## test case for https://jira.taosdata.com:18080/browse/TD-1930
+        tdSql.execute("create table tb(ts timestamp, c1 int, c2 binary(10), c3 nchar(10))")
+        for i in range(10):
+            tdSql.execute("insert into tb values(%d, %d, 'binary%d', 'nchar%d')" % (self.ts + i, i, i, i))
+        
+        tdSql.error("select * from tb where c2 = binary2")
+        tdSql.error("select * from tb where c3 = nchar2")
 
+        tdSql.query("select * from tb where c2 = 'binary2' ")
+        tdSql.checkRows(1)
+
+        tdSql.query("select * from tb where c3 = 'nchar2' ")
+        tdSql.checkRows(1)
+
+        tdSql.query("select * from tb where c1 = '2' ")
+        tdSql.checkRows(1)
+
+        tdSql.query("select * from tb where c1 = 2 ")
+        tdSql.checkRows(1)
 
     def stop(self):
         tdSql.close()
